52a019eb86293c5aa2f4a2bb97303970388416a5
Ammon Lauritzen <allaryin@gmail.com>
2016-02-18 15:46:12 -0600

    Bumped version to 0.1.4

--- src/main/java/org/mcupdater/carnivora/Version.java
+++ src/main/java/org/mcupdater/carnivora/Version.java
@@ -3,7 +3,7 @@ package org.mcupdater.carnivora;
 public class Version {
 	public static final String	MOD_ID		= "Carnivora";
 	public static final String	MOD_NAME	= "Carnivora";
-	public static final String	VERSION		= "0.1.3";
+	public static final String	VERSION		= "0.1.4";
 
 	public static final String	CHANNEL		= MOD_ID;
 	
